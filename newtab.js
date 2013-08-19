28e06472ff7b41827733dd88e3196c483be89e49
Simon Strom <simon.strom@gmail.com>
2013-08-19 20:52:56 +0200

    newtab recent bookmarks

---
+++ newtab.js
@@ -0,0 +1,20 @@
+var bookmarkCollector = {
+  requestBookmarks: function() {
+    chrome.bookmarks.getRecent(20, function(results) {
+      for (var i = 0; i < results.length; i++) {
+        $('#recentBookmarks').append(
+          $('<li>').append(
+            $('<a>').attr('href', results[i].url).append(
+              $('<span>').append(results[i].title)
+              )
+            )
+          );
+      }
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("docuemnt loaded");
+  bookmarkCollector.requestBookmarks();
+});
